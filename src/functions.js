c86e64b79a120ea9a77a0e377cb913d915d96ced
Itamar Yatom <itamary24@gmail.com>
2017-05-22 11:46:05 +0300

    adding documentation to the file

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,17 @@
-// @flow
+/**
+ * Copyright 2017-present, Itamar Yatom.
+ * All rights reserved.
+ *
+ * handles the checks around the functions for the parser.
+ *
+ * the main function checks if there is such a function 
+ * and if there are execatly enough arguments to the fitting function
+ *
+ * @flow
+ */
+
+/* Map<string presesting the name, length: the needed length arguments>
+ * Infinity means any length of arguments */
 const functions = new Map([
 	['cos', 1],
 	['sin', 1],
