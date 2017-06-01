1b63069ac04b69c00d955827c42df001f5be2e07
itamar244 <itamary24@gmail.com>
2017-06-01 11:18:46 +0300

    updating evaluateEquation

--- src/evaluator/index.js
+++ src/evaluator/index.js
@@ -3,10 +3,15 @@ import * as tt from '../tokenizer/types'
 import * as N from '../types'
 import { operators } from '../operators'
 
+// there are some number anomalies at these ocations. the results supposed to be +-Infinity
+// they are the limits of safe numbers in js, so they have their issues
+const ANOMALY_NUMBERS = [
+	26769460249232280,
+	21151178468529210,
+	2 ** 53,
+]
+
 const toFixed = precision => (num) => {
-	if (!precision) {
-		return Number(num)
-	}
 	const res = Math.floor(num * 10 ** precision) / 10 ** precision
 	return Math.abs(res) === Infinity ? +num.toFixed(precision) : res
 }
@@ -17,6 +22,7 @@ export function evaluateEquation(
 	variableName: string,
 	firstTime: bool = true,
 ) {
+	const equation = startingEquation
 	// using mutable object here only for performance
 	const params: { [key: string]: number } = { [variableName]: -1 }
 	// pointing if need to add positive or negative values
@@ -26,9 +32,8 @@ export function evaluateEquation(
 	let change = 1 / 1.5
 	// values for each side of equation
 	let values: number[] = [0, 1]
-	let tmpToFixed = toFixed()
-
-	const equation = startingEquation
+	// the number decreases for each 1000's loop
+	let tmpToFixed = number => number
 
 	// because NaN !== NaN the first run should work even if values is an empty array
 	for (let i = 1; tmpToFixed(values[0]) !== tmpToFixed(values[1]); i += 1) {
@@ -52,30 +57,27 @@ export function evaluateEquation(
 			)
 		}
 	}
-	// there are some number anomalies at these ocations. the results supposed to be +-Infinity
-	// they are the limits of save numbers in js, so they have their issues
+
 	return (
-		[26769460249232280, 21151178468529210, 2 ** 53].some(n => n === Math.abs(params[variableName]))
-		? params[variableName] > 0 ? Infinity : -Infinity
+		ANOMALY_NUMBERS.some(n => n === Math.abs(params[variableName]))
+		? Infinity * (params[variableName] > 0 ? 1 : -1)
 		: params[variableName]
 	)
 }
 
 export const evaluateExpression = (node: N.Node, params: { [string]: number }) => (
-	node.type === 'EXPRESSION'
-	? evaluateExpression(node.body, params)
-	: node.type === tt.LITERAL
+	node.type === tt.LITERAL
 	?	node.value
-	: node.type === tt.CONSTANT
-	? Math[node.name]
 	: node.type === tt.BIN_OPERATOR
 	? operators[node.operator](
 		node.left ? evaluateExpression(node.left, params) : 0,
-		evaluateExpression(node.right, params),
+		node.right ? evaluateExpression(node.right, params) : 0,
 	)
+	: node.type === 'EXPRESSION'
+	? evaluateExpression(node.body, params)
+	: node.type === tt.CONSTANT
+	? Math[node.name]
 	: node.type === tt.FUNCTION
 	? Math[node.name](...node.arguments.map(arg => evaluateExpression(arg, params)))
-	: node.type === tt.PARAM
-	? params[node.name]
-	: 0
+	: params[node.name] // param
 )
